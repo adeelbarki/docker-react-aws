8fdcc4223dea24603ba3dcada93ad0e2c6627a18
Adeel Ahmed Khan <adeelbarki@gmail.com>
2019-06-14 22:22:57 +0500

    single test

--- src/App.test.js
+++ src/App.test.js
@@ -8,9 +8,4 @@ it('renders without crashing', () => {
   ReactDOM.unmountComponentAtNode(div);
 });
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
 
